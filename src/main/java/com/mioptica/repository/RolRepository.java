b3b594da8f6ed212b3909e6a0bacbd268bd247c6
Adrian Mota <mot2025102@itc.edu.gt>
2026-04-07 08:09:24 -0600

    Limpieza total para reestructurar el proyecto

--- src/main/java/com/mioptica/repository/RolRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mioptica.repository;
-
-import com.mioptica.model.Rol;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RolRepository extends JpaRepository<Rol, Integer> { }
